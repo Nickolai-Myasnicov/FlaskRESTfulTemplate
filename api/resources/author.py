5eb6918cd021fcd35d17c29cd825c28f303cc297
nickolai <nickolai.myasnicov@gmail.com>
2021-05-19 20:46:51 +0300

    initial commit

---
+++ api/resources/author.py
@@ -0,0 +1,39 @@
+from api import Resource, reqparse, db
+from api.models.author import AuthorModel
+from api.schemas.author import author_schema, authors_schema
+
+
+class AuthorResource(Resource):
+    def get(self, author_id=None):
+        if author_id is None:
+            authors = AuthorModel.query.all()
+            return authors_schema.dump(authors), 200
+
+        author = AuthorModel.query.get(author_id)
+        if author:
+            return author.to_dict(), 200
+
+        return {"Error:": f"Author id={author_id} not found"}, 404
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", required=True)
+        author_data = parser.parse_args()
+        author = AuthorModel(author_data["name"])
+        db.session.add(author)
+        db.session.commit()
+        return author.to_dict(), 201
+
+    def put(self, author_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", required=True)
+        author_data = parser.parse_args()
+        author = AuthorModel.query.get(author_id)
+        if author is None:
+            author = AuthorModel(author_data["name"])
+            db.session.add(author)
+            db.session.commit()
+            return author.to_dict(), 201
+        author.name = author_data["name"]
+        db.session.commit()
+        return author.to_dict(), 200
