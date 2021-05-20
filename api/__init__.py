53ad31f7cbc3224e61118965e97756923a5e8cd8
nickolai <nickolai.myasnicov@gmail.com>
2021-05-20 21:14:57 +0300

    add auth & token

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,10 @@
 from config import Config
-from flask import Flask, request
+from flask import Flask, request, g
 from flask_restful import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_httpauth import HTTPBasicAuth
 
 
 class UnicodeApi(Api):
@@ -20,3 +21,25 @@ api = UnicodeApi(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    from api.models.user import UserModel
+    print("username_or_token = ", username_or_token)
+    user = UserModel.verify_auth_token(username_or_token)
+    if not user:
+        user = UserModel.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+
+@auth.get_user_roles
+def get_user_roles(data):
+    from api.models.user import UserModel
+    token = data['username']
+    user = UserModel.verify_auth_token(token)
+    return user.get_role()
--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from api import api, app
 from api.resources.quote import QuoteResource
 from api.resources.author import AuthorResource
+from api.resources.user import UserResource
+from api.resources.token import TokenResource
 from config import Config
 
 api.add_resource(QuoteResource,
                  '/authors/<int:author_id>/quotes/<int:quote_id>',
                  '/authors/<int:author_id>/quotes',
                  '/quotes'
-                 )  # <-- requests
+                 )
 api.add_resource(AuthorResource,
                  '/authors/<int:author_id>',
-                 '/authors')  # <-- requests
+                 '/authors')
+
+api.add_resource(UserResource,
+                 '/users', '/users/<int:id>')
+
+api.add_resource(TokenResource, '/auth/token')
 
 if __name__ == '__main__':
     app.run(debug=Config.DEBUG, port=Config.PORT)
